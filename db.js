392f988eb2c7c0d71ab65faa169faceffe4d7ffa
byasun <annaclarag6@gmail.com>
2024-10-28 13:40:24 -0300

    Correção MongoDB

--- db.js
+++ db.js
@@ -1,25 +1,17 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do .env
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI; // Agora ele vai pegar a URI do .env
-
-if (!uri) {
-    console.error('A variável de ambiente MONGODB_URI não está definida.');
-    process.exit(1); // Encerra o processo se a URI não estiver definida
-}
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false, // Evita depreciações
-    useCreateIndex: true, // Para criar índices automaticamente
 })
 .then(() => {
     console.log('Conectado ao MongoDB com sucesso!');
 })
 .catch((error) => {
     console.error('Erro ao conectar ao MongoDB:', error.message);
-    process.exit(1); // Encerra o processo se a conexão falhar
 });
 
 module.exports = mongoose;
