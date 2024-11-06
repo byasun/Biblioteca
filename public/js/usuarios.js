2023dd7912d23d85a52c07884cee96d4737300d8
byasun <annaclarag6@gmail.com>
2024-11-06 07:58:52 -0300

    correção erro de registro de usuario

--- public/js/usuarios.js
+++ public/js/usuarios.js
@@ -1,11 +1,11 @@
 class UsuarioService {
     static async cadastrar(dadosUsuario) {
         try {
-            const response = await fetch('https://biblioteca-regap-7049125ed8fd.herokuapp.com/api/v1/auth/registro', { // URL corrigida da API
+            const response = await fetch('https://biblioteca-regap-7049125ed8fd.herokuapp.com/api/v1/usuarios/registrar', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(dadosUsuario)
-            });
+            });            
 
             // Verifique se a resposta não está OK
             if (!response.ok) {
